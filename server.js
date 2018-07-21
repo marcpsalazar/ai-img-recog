e40a22b28b2c598def935132a04b3668650dfe9e
deasydoesit <darwin.rinderer@gmail.com>
2018-07-21 12:39:56 -0400

    Including react

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send("Hello World!");
-});
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/mern",
+  {
+    useMongoClient: true
+  }
+);
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
