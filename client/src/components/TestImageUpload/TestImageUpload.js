114533bdfb983fccf8119c4d249115cbe2335c1f
deasydoesit <darwin.rinderer@gmail.com>
2018-07-22 19:00:02 -0400

    CCreating image recognition pipeline to Watson

--- client/src/components/TestImageUpload/TestImageUpload.js
+++ client/src/components/TestImageUpload/TestImageUpload.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const TestImageUpload = props => (
   <form>
-    <div class="form-group" method="post" enctype="multipart/form-data" onChange={props.fileChangedHandler}>
-      <label for="exampleFormControlFile1">Test image input</label>
-      <input type="file" class="form-control-file" accept="image/*" id="exampleFormControlFile1"/>
+    <div className="form-group" method="post" encType="multipart/form-data" onChange={props.fileChangedHandler}>
+      <label htmlFor="exampleFormControlFile1">Test image input</label>
+      <input type="file" className="form-control-file" accept="image/*" id="exampleFormControlFile1"/>
       <input type="submit" value="Upload" onClick={props.uploadHandler}/>
     </div>
   </form>
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,11 +1,37 @@
-var multer = require('multer');
+require("dotenv").config(); 
+const fs = require('fs');
+const multer = require('multer');
+const VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+
+const visualRecognition = new VisualRecognitionV3({
+    version: process.env.WATSON_VERSION,
+    iam_apikey: process.env.WATSON_APIKEY
+});
 
 module.exports = function (app) {
 
     let upload = multer({ dest: 'uploads/' });
 
     app.post("/api/image-upload", upload.single('photo'), function (req, res, next) {
+
         console.log(req.file);
+
+        let image_file = fs.createReadStream(req.file.path);
+        const classifier_ids = ["trees_1995546525"];
+        const threshold = 0.6;
+
+        let params = {
+            images_file: image_file,
+            classifier_ids: classifier_ids,
+            threshold: threshold
+        };
+
+        visualRecognition.classify(params, function(err, response) {
+            if (err)
+              console.log(err);
+            else
+              console.log(JSON.stringify(response, null, 2))
+          });
     });
 
 };
