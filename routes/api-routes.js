914f988a33fa94ed58e61f195aeb2d75e4fc55a5
deasydoesit <darwin.rinderer@gmail.com>
2018-07-22 22:43:47 -0400

    Incorporating AWS into image recognition pipeline

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,36 +1,64 @@
 require("dotenv").config(); 
 const fs = require('fs');
+const aws = require('aws-sdk');
+const multerS3 = require('multer-s3');
 const multer = require('multer');
 const VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
 
+// Initiaize Watson Visual Recognition
 const visualRecognition = new VisualRecognitionV3({
     version: process.env.WATSON_VERSION,
     iam_apikey: process.env.WATSON_APIKEY
 });
+//---*
+
+// Initialize AWS
+aws.config.update({
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    region: 'us-east-1'
+});  
+
+const s3 = new aws.S3();
+
+let path = "https://s3.amazonaws.com/leafy-me/public/";
+
+let upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: "leafy-me/public",
+        key: function (req, file, cb) {
+            //console.log(file);
+            let newImage = file.fieldname + Date.now() + ".jpg";
+            path += newImage;
+            cb(null, newImage);
+        }
+    })
+});
+//--*
 
 module.exports = function (app) {
 
-    let upload = multer({ dest: 'uploads/' });
-
+    // Path for image upload
     app.post("/api/image-upload", upload.single('photo'), function (req, res, next) {
 
-        console.log(req.file);
+        console.log(req.file.location); // The image file is availbe through req.file
 
-        let image_file = fs.createReadStream(req.file.path);
+        let image_url =  req.file.location; // Prep the image file for transmission to Watson
         const classifier_ids = ["trees_1995546525"];
         const threshold = 0.6;
 
-        let params = {
-            images_file: image_file,
+        let params = { // Set up Watson parameters
+            url: image_url,
             classifier_ids: classifier_ids,
             threshold: threshold
         };
 
-        visualRecognition.classify(params, function(err, response) {
+        visualRecognition.classify(params, function(err, response) { // Watson request
             if (err)
               console.log(err);
             else
-              console.log(JSON.stringify(response, null, 2))
+              console.log(JSON.stringify(response, null, 2));
           });
     });
 
