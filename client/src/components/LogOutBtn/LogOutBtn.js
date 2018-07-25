5fab500a2baed59f824e310253b037ba5f04a3b4
Yirklum <yirklum@aol.com>
2018-07-25 13:14:04 -0400

    added routes

--- client/src/components/LogOutBtn/LogOutBtn.js
+++ client/src/components/LogOutBtn/LogOutBtn.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./LogOutBtn.css";
 
 const LogOutBtn = props => (
 
-    <button type="button" className="btn btn-success">
-        Log Out
-    </button>
+    <Link to="/">
+        <button type="button" className="btn btn-success">
+            Log Out
+        </button>
+    </Link>
 );
 
 export default LogOutBtn;
