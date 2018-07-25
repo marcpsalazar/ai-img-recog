e0e4d59679f91ac8028dd9790e1947a27b3a0a40
Yirklum <yirklum@aol.com>
2018-07-25 02:26:12 -0400

    stuff

---
+++ client/src/pages/Profile/Profile.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Profile.css";
+import "../TestImageSubmit";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import LogOutBtn from "../../components/LogOutBtn";
+
