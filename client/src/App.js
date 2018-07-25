72ba253fd41de951ea58934c10165a41cd84bcb5
abhilashalk <abhilulk@gmail.com>
2018-07-25 10:53:50 -0400

    Signin and signup forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import TestImageSubmit from "./pages/TestImageSubmit";
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const App = () => (
   <Router>
     <div>
       <Switch>
         <Route exact path="/" component={TestImageSubmit} />
-      </Switch> 
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/signup" component={SignUp} />
+      </Switch>
     </div>
   </Router>
 );
