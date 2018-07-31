2528f10828950ee034546d45aaa4379f089921bb
abhilashalk <abhilulk@gmail.com>
2018-07-31 17:55:09 -0400

    changes in my branch

--- client/src/pages/SignIn/SignIn.js
+++ client/src/pages/SignIn/SignIn.js
@@ -1,115 +1,117 @@
 import React, { Component } from "react";
 import "./SignIn.css";
 import Input from "../../components/Input";
-import {
-  getFromStorage,
-  setInStorage,
-} from '../../utils/storage';
+import { getFromStorage, setInStorage, } from '../../utils/storage';
 import API from "../../utils/API";
 import Profile from "../Profile";
-import {Route, Redirect} from "react-router";
+import { Route, Redirect } from "react-router";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import Container from "../../components/Container";
+
 
 
 
 class SignIn extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            token: '',
-            signInUser: '',
-            signInPass: '',
-            fireRedirect: false
-        }
-
-        this.HandleInputChangeSignInPass = this.HandleInputChangeSignInPass.bind(this);
-        this.HandleInputChangeSignInUser = this.HandleInputChangeSignInUser.bind(this);
-        this.onSignIn = this.onSignIn.bind(this);
-    }
-    HandleInputChangeSignInUser(event) {
-      this.setState({
-        signInUser: event.target.value
-
-      })
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: '',
+      signInUser: '',
+      signInPass: '',
+      fireRedirect: false
     }
 
-    HandleInputChangeSignInPass(event) {
-      this.setState({
-        signInPass: event.target.value
+    this.HandleInputChangeSignInPass = this.HandleInputChangeSignInPass.bind(this);
+    this.HandleInputChangeSignInUser = this.HandleInputChangeSignInUser.bind(this);
+    this.onSignIn = this.onSignIn.bind(this);
+  }
+  HandleInputChangeSignInUser(event) {
+    this.setState({
+      signInUser: event.target.value
 
-      })
+    })
+  }
+
+  HandleInputChangeSignInPass(event) {
+    this.setState({
+      signInPass: event.target.value
+
+    })
+  }
+
+  onSignIn(e) {
+    e.preventDefault()
+    const { signInUser, signInPass } = this.state
+
+    let siObj = {
+      username: signInUser,
+      password: signInPass
     }
 
-    onSignIn(e) {
-      e.preventDefault()
-      const {
-        signInUser,
-        signInPass
-      } = this.state
-
-      let siObj = {
-        username: signInUser,
-        password: signInPass
-      }
-
-      API.signIn(siObj)
+    API.signIn(siObj)
 
       .then(json => {
         console.log(json)
-          if (json.statusText==="OK") {
-            console.log('storage method reached')
-            console.log(json.data.token)
-            setInStorage('the_main-app', { token: json.data.token });
-            this.setState({
-              signInError: json.data.message,
-              isLoading: false,
-              signInUser: '',
-              signInPass: '',
-              token: json.data.token
-            })
-            // console.log(this.state.token)
-          }  else {
-            this.setState({
-              signInError: json.message,
-              isLoading: false
-            })
-          }
-                        this.setState({fireRedirect: true});
+        if (json.statusText === "OK") {
+          console.log('storage method reached')
+          console.log(json.data.token)
+          setInStorage('the_main-app', { token: json.data.token });
+          this.setState({
+            signInError: json.data.message,
+            isLoading: false,
+            signInUser: '',
+            signInPass: '',
+            token: json.data.token
+          })
+          // console.log(this.state.token)
+        } else {
+          this.setState({
+            signInError: json.message,
+            isLoading: false
+          })
+        }
+        this.setState({ fireRedirect: true });
 
-        })
+      })
 
-    }
+  }
 
-    render() {
-      const {
-        token,
-        signInError,
-        signInUser,
-        signInPass,
-        fireRedirect
-      } = this.state;
-        return (
-            <div>
-                <form className="signIn-form">
-                    <h3 className="signin-heading"> Hello </h3>
-                    <Input
-                        type="text"
-                        placeholder="Username"
-                        value={signInUser}
-                        onChange={this.HandleInputChangeSignInUser}/>
-                    <Input
-                        type="password"
-                        placeholder="Password"
-                        value={signInPass}
-                        onChange={this.HandleInputChangeSignInPass}/>
-                    <br />
-                    <button onClick={this.onSignIn}>Sign In</button>
-                </form>
-                {fireRedirect && (
-                  <Redirect to={'/profile'}/>
-                  )}
-            </div>
-        );
-    }
+  render() {
+    const {
+      token,
+      signInError,
+      signInUser,
+      signInPass,
+      fireRedirect
+    } = this.state;
+    return (
+      <Container fluid>
+        <div className="background-image">
+          <Header />
+          <form className="signIn-form">
+            <h2 className="signin-heading"> Hello </h2>
+            <Input
+              type="text"
+              placeholder="Username"
+              value={signInUser}
+              onChange={this.HandleInputChangeSignInUser} />
+            <Input
+              type="password"
+              placeholder="Password"
+              value={signInPass}
+              onChange={this.HandleInputChangeSignInPass} />
+            <br />
+            <button id="signin" onClick={this.onSignIn}>Sign In</button>
+          </form>
+          {fireRedirect && (
+            <Redirect to={'/profile'} />
+          )}
+          <Footer />
+        </div>
+      </Container>
+    );
+  }
 
 }
 
--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -1,89 +1,94 @@
 import React, { Component } from "react";
-import {Route, Redirect} from "react-router";
+import { Route, Redirect } from "react-router";
 import "./SignUp.css";
 import Profile from "../Profile";
 import Input from "../../components/Input";
 import API from "../../utils/API";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import Container from "../../components/Container";
 
 class SignUp extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            token: '',
-            signUpUser: '',
-            signUpPass: '',
-            fireRedirect: false
-        }
-
-        this.HandleInputChangeSignUpUser = this.HandleInputChangeSignUpUser.bind(this);
-        this.HandleInputChangeSignUpPass = this.HandleInputChangeSignUpPass.bind(this);
-        this.onSignUp = this.onSignUp.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: '',
+      signUpUser: '',
+      signUpPass: '',
+      fireRedirect: false
     }
-      HandleInputChangeSignUpUser(event) {
-        this.setState({
-          signUpUser: event.target.value
 
-        })
-      }
+    this.HandleInputChangeSignUpUser = this.HandleInputChangeSignUpUser.bind(this);
+    this.HandleInputChangeSignUpPass = this.HandleInputChangeSignUpPass.bind(this);
+    this.onSignUp = this.onSignUp.bind(this);
+  }
+  HandleInputChangeSignUpUser(event) {
+    this.setState({
+      signUpUser: event.target.value
 
-      HandleInputChangeSignUpPass(event) {
-        this.setState({
-          signUpPass: event.target.value
+    })
+  }
 
-        })
-      }
+  HandleInputChangeSignUpPass(event) {
+    this.setState({
+      signUpPass: event.target.value
 
-      onSignUp(e) {
-        e.preventDefault()
-        this.setState({fireRedirect: true});
-        const {
-          signUpUser,
-          signUpPass
-        } = this.state
-        console.log(signUpUser + signUpPass);
-        let suObj = {
-          username: signUpUser,
-          password: signUpPass
-        }
-        API.signUp(suObj);
+    })
+  }
+
+  onSignUp(e) {
+    e.preventDefault()
+    this.setState({ fireRedirect: true });
+    const {
+      signUpUser,
+      signUpPass
+    } = this.state
+    console.log(signUpUser + signUpPass);
+    let suObj = {
+      username: signUpUser,
+      password: signUpPass
+    }
+    API.signUp(suObj);
+  }
+
+
+
+  render() {
+    const {
+      signUpUser,
+      signUpPass,
+      fireRedirect
+    } = this.state;
+    const { from } = this.props.location.state || '/'
+
+    return (
+      <Container fluid>
+        <div className="background-image">
+          <Header />
+          <form className="signUp-form">
+            <h3 className="signup-heading"> Create a Profile </h3>
+            <Input
+              type="text"
+              placeholder="Username"
+              value={signUpUser}
+              onChange={this.HandleInputChangeSignUpUser} />
+            <Input
+              type="password"
+              placeholder="Password"
+              value={signUpPass}
+              onChange={this.HandleInputChangeSignUpPass} />
+            <br />
+            <button id="signup" onClick={this.onSignUp}>Sign Up</button>
+          </form>
+          {fireRedirect && (
+            <Redirect to={'/profile'} />
+          )}
+          <Footer />
+        </div>
+      </Container>
+    );
+  }
 }
-
-
-
-
-
-
-    render() {
-      const {
-        signUpUser,
-        signUpPass,
-        fireRedirect
-      } = this.state;
-      const { from } = this.props.location.state || '/'
-
-        return (
-              <div>
-                <form className="signUp-form">
-                    <h3 className="signup-heading"> Create a Profile </h3>
-                    <Input
-                        type="text"
-                        placeholder="Username"
-                        value={signUpUser}
-                        onChange={this.HandleInputChangeSignUpUser}/>
-                    <Input
-                        type="password"
-                        placeholder="Password"
-                        value={signUpPass}
-                        onChange={this.HandleInputChangeSignUpPass}/>
-                    <br />
-                    <button onClick={this.onSignUp}>Sign Up</button>
-                </form>
-                {fireRedirect && (
-                  <Redirect to={'/signin'}/>
-                  )}
-              </div>);
-      }
-    }
 
 
 
